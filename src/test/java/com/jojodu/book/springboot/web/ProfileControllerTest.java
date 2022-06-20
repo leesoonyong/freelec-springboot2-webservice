cbfe468c6c4abf539741ee12854f6ce23def84f8
lee <wnghkdcjdtns@naver.com>
2022-06-20 22:42:12 +0900

    무중단 배포 Profile Api 구현

--- src/test/java/com/jojodu/book/springboot/web/ProfileControllerTest.java
+++ src/test/java/com/jojodu/book/springboot/web/ProfileControllerTest.java
@@ -2,12 +2,27 @@ package com.jojodu.book.springboot.web;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.env.MockEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.assertj.core.api.Assertions.*;
 
 
 public class ProfileControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     public void real_profile이_조회된다(){
@@ -56,6 +71,9 @@ public class ProfileControllerTest {
         //then
         assertThat(profile).isEqualTo(expectedProfile);
     }
+
+
+
 
 
 
